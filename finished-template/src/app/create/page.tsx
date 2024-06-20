197a8a917e0622083db85f1f2602026141e2e387
dung204 <acezombiev4@gmail.com>
2024-06-20 21:45:15 +0700

    feat: create post page

--- finished-template/src/app/create/page.tsx
+++ finished-template/src/app/create/page.tsx
@@ -1,7 +1,36 @@
-export default function Page() {
+import { redirect } from 'next/navigation';
+
+export default function CreatePost() {
   return (
     <section>
-      <h1>Create page</h1>
+      <form action={addPost}>
+        <label htmlFor='title'>Title:</label>
+        <input type='text' name='title' id='title' required />
+        <br />
+        <label htmlFor='content'>Content</label>
+        <textarea name='content' id='content' required></textarea>
+        <br />
+        <button type='submit'>Submit</button>
+      </form>
     </section>
   );
+}
+
+async function addPost(formData: FormData) {
+  'use server';
+
+  const post = {
+    title: formData.get('title'),
+    content: formData.get('content'),
+  };
+
+  await fetch('http://127.0.0.1:8090/api/collections/posts/records/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(post),
+  });
+
+  redirect('/posts');
 }
